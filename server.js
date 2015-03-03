930aa8abd9d8d1ece53269cb6d7e1ab40928421b
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-03-03 14:21:42 -0700

    bind server port

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ var faker = require('faker');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5100)
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello From all of us at Empire Carpet.')
+})
+
 app.get('/random-user', function(req, res) {
   var user = faker.helpers.userCard();
   user.avatar = faker.image.avatar();
   res.json(user)
 })
 
-app.listen(3100, function() {
-  console.log('server-status')
+app.listen(app.get('port') function() {
+  console.log('Your app is now running at localhost' + app.get('port') +' HELLO!')
 })
