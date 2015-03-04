a0addd1f689b883b041b27060e86b647805563f2
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-03-03 22:44:00 -0700

    Added modules cors on the server * did a basic front-end for the applicaiton - * minor angular application that pulls the data from the node server

---
+++ public/javascript/application.js
@@ -0,0 +1,46 @@
+(function() {
+  'use strict';
+
+  var App = angular.module('App', []);
+
+  App.constant('API_URL', 'http://localhost:5100/'  )
+
+  App.factory('RandomUserFactory', function RandomUserFactory($http, API_URL) {
+
+    function getUser() {
+      return $http.get(API_URL + 'random-user')
+    }
+    function getCompany() {
+      return $http.get(API_URL + 'random-company')
+    }
+
+    return {
+      getUser: getUser,
+      getCompany: getCompany
+    } 
+
+  })
+
+  App.controller('MainCtrl', ['RandomUserFactory', 'API_URL',function(RandomUserFactory, API_URL) {
+    'use strict';
+    var main = this;
+    main.home = API_URL;
+
+    var getRandomUser = function() {
+        RandomUserFactory.getUser().then(function success(response) {
+          main.randomUser = response.data;
+        })
+    }
+
+    var getRandomCompany = function() {
+        RandomUserFactory.getCompany().then( function success(response) {
+          main.randomCompany = response.data;
+        })
+    }
+ 
+    main.getRandomUser = getRandomUser;
+    main.getRandomCompany = getRandomCompany;
+
+  }]);
+  
+})();
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var faker = require('faker');
+var cors = require('cors');
 
 var app = express();
+app.use(cors());
 
 app.set('port', (process.env.PORT || 5100));
 app.use(express.static(__dirname + '/public'))
@@ -14,6 +16,20 @@ app.get('/random-user', function(req, res) {
   var user = faker.helpers.userCard();
   user.avatar = faker.image.avatar();
   res.json(user)
+})
+
+app.get('/random-company', function(req, resp) {
+  var company = function() {
+    return {
+      "name" : faker.company.companyName(),
+      "motto": faker.company.catchPhrase(),
+      "long-name": faker.company.bs()
+    }
+  }
+
+  var co = company()
+  co.years_established = Math.floor((Math.random() * 100) + 1)
+  resp.json(co);
 })
 
 app.listen(app.get('port'), function() {
