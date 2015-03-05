5a62153e6c725aec570ce05d078b4c3fd41e9a0d
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-03-04 17:14:56 -0700

    Created an Interceptor to pass the Bearer Token with every request Modified the UserSession Factory incorporate this interceptor on login and logout

--- public/javascript/factories.js
+++ public/javascript/factories.js
@@ -14,11 +14,38 @@ App.factory('RandomUserFactory', function RandomUserFactory($http, API_URL) {
 
 })
 
-App.factory('UserSession', function UserSession($http, API_URL) {
+App.factory('AuthTokenStore', function AuthTokenStore($window) {
+  'use strict';
+
+  var store = $window.localStorage;
+  var key = 'auth-token';
+
+  return {
+    getToken: getToken,
+    setToken: setToken
+  }
+
+  function  getToken() {
+    return store.getItem(key);
+  }
+
+  function setToken(token) {
+    if(token) {
+      store.setItem(key, token)
+    } else {
+      store.removeItem(key); 
+    }
+  }
+})
+
+
+App.factory('UserSession', function UserSession($http, API_URL, AuthTokenStore) {
 
   var state = {
     login: login,
-    session: { error: null }
+    logout: logout,
+    session: { error: null },
+    user: null
   }
 
   function login(username, password) {
@@ -26,8 +53,37 @@ App.factory('UserSession', function UserSession($http, API_URL) {
       username: username,
       password: password
     })
+    .then(function success(response) {
+      console.log('token', response.data.token )
+      AuthTokenStore.setToken(response.data.token);
+      return response;
+    })
   }
 
+  function logout() {
+    AuthTokenStore.setToken();
+  }
+  
   return state;
+})
 
+
+App.factory('AuthInterceptor', function AuthInterceptor(AuthTokenStore) {
+  'use strict';
+  return {
+    request: AddToken
+  }
+
+  function AddToken(config) {
+    var token = AuthTokenStore.getToken()
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = 'Bearer ' + token;
+    }
+    return config;
+  }
+})
+
+App.config( function($httpProvider) {
+  $httpProvider.interceptors.push('AuthInterceptor');
 })
