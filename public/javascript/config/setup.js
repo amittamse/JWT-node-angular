8507892a3c6de2d13fe478601959cf922d6f28e5
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-03-04 19:20:20 -0700

    modified root API_URL on apptwo

--- public/javascript/config/setup.js
+++ public/javascript/config/setup.js
@@ -1,4 +1,4 @@
 var App = angular.module('App', []);
 
 // App.constant('API_URL',  window.location.protocol + '//' + window.location.host + window.location.pathname  )
-App.constant('API_URL',  'http://localhost:5100/ ')
+App.constant('API_URL',  'https://stormy-beyond-9592.herokuapp.com/')
